dcb741d10102a3cbc372316a1259f5d0622382ec
mrtn <sirmartinsteen@gmail.com>
2026-07-05 11:20:02 +0200

    Updated API with getting historic data - and updated temperature.html with possibility to render this

--- src/main/kotlin/club/mascotfan/temperature_api/controller/TemperatureController.kt
+++ src/main/kotlin/club/mascotfan/temperature_api/controller/TemperatureController.kt
@@ -1,10 +1,14 @@
 package club.mascotfan.temperature_api.controller
 
+import club.mascotfan.temperature_api.controller.model.HistoricDataResponse
 import club.mascotfan.temperature_api.controller.model.TemperatureResponse
 import club.mascotfan.temperature_api.db.SQLTemperatureRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
 
 @RestController
 class TemperatureController(
@@ -15,6 +19,28 @@ class TemperatureController(
     fun temperature(): ResponseEntity<TemperatureResponse> {
         try {
             return ResponseEntity.ok(sqlTemperatureRepository.getLastTemperatures())
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return ResponseEntity.internalServerError().build()
+        }
+    }
+
+    @GetMapping("/temperature/source/{source}")
+    fun getTempsForSourceDuringPeriod(
+        @PathVariable source: String,
+        @RequestParam(value = "fromTimestamp", required = false) _fromTimestamp: Instant?,
+        @RequestParam(value = "toTimestamp", required = false) _toTimestamp: Instant?
+    ): ResponseEntity<HistoricDataResponse> {
+        val fromTimestamp = _fromTimestamp ?: Instant.now().minusSeconds(60 * 60 * 24 * 7) // default to 7 days ago
+        val toTimestamp = _toTimestamp ?: Instant.now()
+        try {
+            return ResponseEntity.ok(
+                sqlTemperatureRepository.getHistoricDataFromSource(
+                    source,
+                    fromTimestamp,
+                    toTimestamp
+                )
+            )
         } catch (e: Exception) {
             e.printStackTrace()
             return ResponseEntity.internalServerError().build()
--- src/main/kotlin/club/mascotfan/temperature_api/db/SQLTemperatureRepository.kt
+++ src/main/kotlin/club/mascotfan/temperature_api/db/SQLTemperatureRepository.kt
@@ -1,10 +1,13 @@
 package club.mascotfan.temperature_api.db
 
+import club.mascotfan.temperature_api.controller.model.HistoricDataResponse
 import club.mascotfan.temperature_api.controller.model.TemperatureResponse
 import club.mascotfan.temperature_api.model.HumidityMessage
 import club.mascotfan.temperature_api.model.TemperatureMessage
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
+import java.sql.Timestamp
+import java.time.Instant
 import java.time.OffsetDateTime
 import java.time.ZoneId
 
@@ -51,8 +54,78 @@ class SQLTemperatureRepository(private val namedParameterJdbcTemplate: NamedPara
         )
     }
 
+    fun getHistoricDataFromSource(
+        source: String,
+        fromTimestamp: Instant,
+        toTimestamp: Instant
+    ): HistoricDataResponse {
+        return HistoricDataResponse(
+            source = source,
+            temperatures = getHistoricTemperatureDataFromSource(source, fromTimestamp, toTimestamp),
+            humidity = getHistoricHumidityDataFromSource(source, fromTimestamp, toTimestamp),
+            fromTimestamp = OffsetDateTime.ofInstant(fromTimestamp, ZoneId.of("Europe/Oslo")),
+            toTimestamp = OffsetDateTime.ofInstant(toTimestamp, ZoneId.of("Europe/Oslo"))
+        )
+    }
+
+    private fun getHistoricTemperatureDataFromSource(
+        source: String,
+        fromTimestamp: Instant,
+        toTimestamp: Instant
+    ): List<HistoricDataResponse.Data> {
+        return namedParameterJdbcTemplate.query(
+            """
+                select * from data.temperatures
+                WHERE  source = :source AND recorded_at >= :from_timestamp AND recorded_at < :to_timestamp
+                ORDER BY recorded_at 
+            """.trimIndent(),
+            mapOf(
+                "source" to source,
+                "from_timestamp" to Timestamp.from(fromTimestamp),
+                "to_timestamp" to Timestamp.from(toTimestamp)
+            )
+        )
+        { rs, _ ->
+            HistoricDataResponse.Data(
+                value = rs.getString("temperature_celsius")?.toDouble() ?: 0.0,
+                recorded = OffsetDateTime.ofInstant(
+                    rs.getTimestamp("recorded_at")?.toInstant(),
+                    ZoneId.of("Europe/Oslo")
+                )
+            )
+        }
+    }
+
+    fun getHistoricHumidityDataFromSource(
+        source: String,
+        fromTimestamp: Instant,
+        toTimestamp: Instant
+    ): List<HistoricDataResponse.Data> {
+        return namedParameterJdbcTemplate.query(
+            """
+                select * from data.humidity
+                WHERE  source = :source AND recorded_at >= :from_timestamp AND recorded_at < :to_timestamp
+                ORDER BY recorded_at 
+            """.trimIndent(),
+            mapOf(
+                "source" to source,
+                "from_timestamp" to Timestamp.from(fromTimestamp),
+                "to_timestamp" to Timestamp.from(toTimestamp)
+            )
+        )
+        { rs, _ ->
+            HistoricDataResponse.Data(
+                value = rs.getString("humidity_percentage")?.toDouble() ?: 0.0,
+                recorded = OffsetDateTime.ofInstant(
+                    rs.getTimestamp("recorded_at")?.toInstant(),
+                    ZoneId.of("Europe/Oslo")
+                )
+            )
+        }
+    }
+
     private fun getLastTemperaturesForAllSources(): Map<String, TemperatureResponse.Sources.Temperature> {
-        val temperatures : MutableMap<String, TemperatureResponse.Sources.Temperature> = mutableMapOf()
+        val temperatures: MutableMap<String, TemperatureResponse.Sources.Temperature> = mutableMapOf()
         namedParameterJdbcTemplate.query(
             """
                 SELECT DISTINCT ON (source) source, temperature_celsius, recorded_at
@@ -61,7 +134,7 @@ class SQLTemperatureRepository(private val namedParameterJdbcTemplate: NamedPara
             """.trimIndent()
         ) { rs, _ ->
             val source = rs.getString("source") ?: "UNKNOWN"
-            temperatures[source] =  TemperatureResponse.Sources.Temperature(
+            temperatures[source] = TemperatureResponse.Sources.Temperature(
                 rs.getString("temperature_celsius")?.toDouble() ?: 0.0,
                 OffsetDateTime.ofInstant(rs.getTimestamp("recorded_at")?.toInstant(), ZoneId.of("Europe/Oslo"))
             )
@@ -71,7 +144,7 @@ class SQLTemperatureRepository(private val namedParameterJdbcTemplate: NamedPara
     }
 
     private fun getLastHumdityForAllSources(): Map<String, TemperatureResponse.Sources.Humidity> {
-        val humidity : MutableMap<String, TemperatureResponse.Sources.Humidity> = mutableMapOf()
+        val humidity: MutableMap<String, TemperatureResponse.Sources.Humidity> = mutableMapOf()
         namedParameterJdbcTemplate.query(
             """
                 SELECT DISTINCT ON (source) source, humidity_percentage, recorded_at
