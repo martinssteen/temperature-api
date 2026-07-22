15b0661b4bbd70e7b9f02d097375f0ce8a67ce2e
mrtn <sirmartinsteen@gmail.com>
2026-07-22 10:27:27 +0200

    added ping

---
+++ src/main/kotlin/club/mascotfan/home/shared/PingController.kt
@@ -0,0 +1,12 @@
+package club.mascotfan.home.shared
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class PingController {
+    @GetMapping("/ping")
+    fun ping(): String {
+        return "pong"
+    }
+}
